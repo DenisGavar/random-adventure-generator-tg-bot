51498c31665d460e885547b7d13810adb7708a6e
DenisGavar <tskhelp@gmail.com>
2024-12-02 22:23:14 +0200

    webhook added

--- bot.py
+++ bot.py
@@ -1,19 +1,31 @@
 import os
 from dotenv import load_dotenv
-from telegram.ext import ApplicationBuilder
+from flask import Flask, request
+from telegram import Update, Bot
+from telegram.ext import Application
 from app.handlers.categories import categories_handler
-#from app.handlers.start import start_handler
 
 load_dotenv(override=True)
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+PORT = os.getenv("PORT")
 
-def main():
-    app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+app = Flask(__name__)
 
-    app.add_handler(categories_handler)
-    #app.add_handler(start_handler)
+application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+application.add_handler(categories_handler)
 
-    app.run_polling()
+@app.route(f"/{TELEGRAM_BOT_TOKEN}", methods=["POST"])
+def webhook():
+    json_update = request.get_json()
+    update = Update.de_json(json_update, application.bot)
+    application.update_queue().put(update)
+    return "OK", 200
+
+@app.before_first_request
+def setup_webhook():
+    webhook_url = f"{WEBHOOK_URL}/{TELEGRAM_BOT_TOKEN}"
+    application.bot.set_webhook(url=webhook_url)
 
 if __name__ == "__main__":
-    main()
+    app.run(port=PORT)
