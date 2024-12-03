b385c25e2852d4cc0fe1b27acc466cd29cc99e42
DenisGavar <tskhelp@gmail.com>
2024-12-03 15:33:37 +0200

    simple bot

--- app/services/api_service.py
+++ app/services/api_service.py
@@ -7,9 +7,7 @@ API_BASE_URL = os.getenv("API_BASE_URL")
 
 def fetch_categories():
     try:
-        print("start")
         response = requests.get(f"{API_BASE_URL}/categories")
-        print("got")
         response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
--- bot.py
+++ bot.py
@@ -1,39 +1,17 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, request
-from telegram import Update
 from telegram.ext import Application
 from app.handlers.categories import categories_handler
 
 load_dotenv(override=True)
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-PORT = os.getenv("PORT")
 
-app = Flask(__name__)
+def main():
+    application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
-application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
-application.add_handler(categories_handler)
+    application.add_handler(categories_handler)
 
-@app.route('/setwebhook', methods=['GET'])
-def set_webhook():
-    webhook_url = f"{WEBHOOK_URL}/{TELEGRAM_BOT_TOKEN}"
-    s = application.bot.set_webhook(url=webhook_url)
-    if s:
-        return "Webhook setup successful!"
-    else:
-        return "Webhook setup failed.", 500
-
-@app.route(f"/{TELEGRAM_BOT_TOKEN}", methods=["POST"])
-def webhook():
-    json_update = request.get_json()
-    try:
-        update = Update.de_json(json_update, application.bot)
-        application.update_queue.put_nowait(update)
-        return "OK", 200
-    except Exception as e:
-        print(f"Error handling update: {e}")
-        return "Internal Server Error", 500
+    application.run_polling()
 
 if __name__ == "__main__":
-    app.run(port=PORT)
+    main()
