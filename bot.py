870f69a3d02e4e3b812bb54d5ec5cb11cb9e7a1f
DenisGavar <tskhelp@gmail.com>
2024-12-03 16:13:44 +0200

    simple bot

--- bot.py
+++ bot.py
@@ -25,3 +25,43 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+import os
+from dotenv import load_dotenv
+from flask import Flask, request
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+from app.handlers.categories import categories_handler
+
+load_dotenv(override=True)
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+PORT = os.getenv("PORT", 5000)
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Initialize the Telegram bot application
+bot_app = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+
+# Add the /start handler
+bot_app.add_handler(categories_handler)
+
+@app.route("/", methods=["POST"])
+def webhook():
+    """Endpoint to receive webhook updates from Telegram."""
+    json_data = request.get_json()
+    update = Update.de_json(json_data, bot_app.bot)
+    bot_app.update_queue.put(update)
+    return "OK", 200
+
+@app.route("/set_webhook", methods=["GET"])
+def set_webhook():
+    """Endpoint to manually set the webhook."""
+    bot_app.bot.set_webhook(url=WEBHOOK_URL)
+    return f"Webhook set to {WEBHOOK_URL}", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(PORT))
